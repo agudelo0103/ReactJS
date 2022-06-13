1bbca4c9ca7c2b68059c56d915958becde0b5094
agudelo0103 <crisagudavid@gmail.com>
2022-06-13 15:38:54 -0500

    guardado

--- src/components/card.js
+++ src/components/card.js
@@ -14,13 +14,6 @@ class Card extends React.Component{
         }
     }
 
-    componentDidMount(){
-        setTimeout(() =>{
-             this.setState({
-                image: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06'
-             })
-        }, 5000)
-    }
     render(){
         const  { tittle, description, img, leftColor, rightColor} = this.props
         return (
